b927f5feac902fdd17931a5a34da4ae3e2ab61ce
Johan Smarius <jsmarius@hotmail.com>
2021-02-28 19:37:57 +0100

    Added ctor for injection demo Renamed contracts file

--- GrpcServiceFromCode/Services/EchoService.cs
+++ GrpcServiceFromCode/Services/EchoService.cs
@@ -1,10 +1,18 @@
 ﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using ServiceContracts;
 
 namespace GrpcServiceFromCode
 {
     public class EchoService : IEchoService
     {
+        private readonly ILogger<EchoService> _logger;
+
+        public EchoService(ILogger<EchoService> logger)
+        {
+            _logger = logger;
+        }
+
         public ValueTask<EchoResponse> EchoMessage(EchoRequest request)
         {
             var response = new EchoResponse()
